d9be01f9c2b222820028a506ba1dab38aa0bc49e
webNekit <mochalov-hunter@mail.ru>
2024-03-07 13:01:16 +0300

    Начал делать секцию "О нас" добавил иконки, текст

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { FaUsers } from 'react-icons/fa';
+import { IoIosPricetag } from 'react-icons/io';
+import { FaDumbbell } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+// import main components
+import { fadeIn } from '@/utils/variantsMotion';
 
 const About = () => {
+
+  const featured = [
+    {
+      icon: <FaUsers />,
+      title: 'Ответственные тренера',
+      subtitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati rerum ex asperiores nulla labore?'
+    }
+  ];
+
   return (
     <section className="h-screen bg-yellow-300" id='about'>
-      about section
+      
     </section>
   )
 }
