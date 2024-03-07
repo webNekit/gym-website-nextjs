cd4ff3b566a3c0fc799f8d4d2d429935ec7da52d
webNekit <mochalov-hunter@mail.ru>
2024-03-07 23:58:14 +0300

    Начал делать верстку секциюю о нас

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,17 +9,45 @@ import { fadeIn } from '@/utils/variantsMotion';
 
 const About = () => {
 
-  const featured = [
+  const featureds = [
     {
       icon: <FaUsers />,
       title: 'Ответственные тренера',
       subtitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati rerum ex asperiores nulla labore?'
-    }
+    },
+    {
+      icon: <IoIosPricetag />,
+      title: 'Ответственные тренера',
+      subtitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati rerum ex asperiores nulla labore?'
+    },
+    {
+      icon: <FaDumbbell />,
+      title: 'Ответственные тренера',
+      subtitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati rerum ex asperiores nulla labore?'
+    },
   ];
 
   return (
-    <section className="h-screen bg-yellow-300" id='about'>
-      
+    <section className="pt-8 pb-16 lg:pt-16 lg:pb-28" id='about'>
+      <div className="container mx-auto">
+        <div className="w-full flex flex-col items-center gap-2 mb-6">
+          <h2 className="h2 text-center">О WellFit</h2>
+          <p className="max-w-[600px] mx-auto text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta veritatis unde voluptas non, cum quis autem! Quam nam inventore optio fugit fugiat laudantium nulla quaerat.</p>
+        </div>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {featureds.map((featured, index) => {
+            return (
+              <div key={index}>
+                <div className='text-4xl bg-primary-300 text-white w-[80px] h-[80px] rounded-full flex justify-center items-center'>{featured.icon}</div>
+                <div>
+                  <h3 className='h4 text-accent'>{featured.title}</h3>
+                  <p>{featured.subtitle}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </section>
   )
 }
