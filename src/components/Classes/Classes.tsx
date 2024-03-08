3d27c997a829621b147cab996a5c64e01a1d972f
webNekit <mochalov-hunter@mail.ru>
2024-03-08 21:08:17 +0300

    Сделал секцию классы

--- src/components/Classes/Classes.tsx
+++ src/components/Classes/Classes.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 
 // import main components
 import { fadeIn } from '@/utils/variantsMotion';
+import CustomButton from '../CustomButton/CustomButton';
 
 const classes = [
   {
@@ -31,8 +32,25 @@ const classes = [
 
 const Classes = () => {
   return (
-    <section className="w-full h-screen bg-green-300" id='classes'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, porro!
+    <section className="w-full" id='classes'>
+        <motion.div variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.2 }} className="w-full grid grid-cols-1 lg:grid-cols-2">
+          {classes.map(( item, index ) => {
+            return (
+              <>
+                <div className='relative w-full h-[300px] lg:h-[485px] flex flex-col ' key={index}>
+                  <div className='bg-black/60 absolute w-full h-full top-0 z-10 flex flex-col justify-center items-center gap-4 p-4 lg:p-12'>
+                    <h3 className='h3 text-accent uppercase'>{item.name}</h3>
+                    <p className='text-white text-xl text-center'>{item.description}</p>
+                    <div>
+                      <CustomButton containerStyles='p-4' text='Подробнее'/>
+                    </div>
+                  </div>
+                  <Image fill src={item.img} alt="Изображение" className='object-cover' />
+                </div>
+              </>
+            )
+          })}
+        </motion.div>
     </section>
   )
 }
