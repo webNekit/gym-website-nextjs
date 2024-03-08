851cea85a6a180088bc1594b885d5f4c2c045010
webNekit <mochalov-hunter@mail.ru>
2024-03-08 22:59:37 +0300

    Создал массив с объектами для передачи в map

--- src/components/MemberShip/MemberShip.tsx
+++ src/components/MemberShip/MemberShip.tsx
@@ -5,6 +5,7 @@ const MemberShip = () => {
   return (
     <section className='w-full py-16 bg-membership bg-center bg-cover bg-no-repeat' id="memberShip">
         <div className="container mx-auto">
+            <h2 className="h2 text-white text-center">Программа тренировок</h2>
             
         </div>
     </section>
--- src/components/MemberShipSlider/MemberShipSlider.tsx
+++ src/components/MemberShipSlider/MemberShipSlider.tsx
@@ -1,10 +1,82 @@
 "use client";
 import React from 'react';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import { MdClose } from 'react-icons/md';
+import { FaCheck } from 'react-icons/fa';
+// import main components
+import CustomButton from '../CustomButton/CustomButton';
+// swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const MemberShipSlider = () => {
+
+  const memberShipData = [
+    {
+      title: 'Standart',
+      price: 1200,
+      benefits: [
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+      ]
+    },
+    {
+      title: 'Standart',
+      price: 1200,
+      benefits: [
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+      ]
+    },
+    {
+      title: 'Standart',
+      price: 1200,
+      benefits: [
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+        {
+          icon: FaCheck,
+          text: "Include memberShips"
+        },
+      ]
+    },
+  ];
+
   return (
-    <div>MemberShipSlider</div>
+    <Swiper>
+      <SwiperSlide>1</SwiperSlide>
+      <SwiperSlide>2</SwiperSlide>
+      <SwiperSlide>3</SwiperSlide>
+    </Swiper>
   )
 }
 
-export default MemberShipSlider
+export default MemberShipSlider;
