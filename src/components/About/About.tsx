28c3147e3fc9689398240370120fe0fa338c03fa
webNekit <mochalov-hunter@mail.ru>
2024-03-08 09:17:26 +0300

    Сделал счетчик чисел для разедла "достижения"

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,14 +1,16 @@
 "use client";
-import React from 'react';
-import { FaUsers } from 'react-icons/fa';
+import React, { useRef } from 'react';
+import { FaBriefcase, FaClock, FaTrophy, FaUsers } from 'react-icons/fa';
 import { IoIosPricetag } from 'react-icons/io';
 import { FaDumbbell } from 'react-icons/fa';
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 // import main components
 import { fadeIn } from '@/utils/variantsMotion';
+import CountUp from 'react-countup';
 
 const About = () => {
 
+  // Преимущества
   const featureds = [
     {
       icon: <FaUsers />,
@@ -26,6 +28,32 @@ const About = () => {
       subtitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati rerum ex asperiores nulla labore?'
     },
   ];
+  // Достижения
+  const stats = [
+    {
+      number: 19,
+      icon: FaBriefcase,
+      text: "Залов в Москве"
+    },
+    {
+      number: 879,
+      icon: FaClock,
+      text: "Рабочих часов"
+    },
+    {
+      number: 150,
+      icon: FaUsers,
+      text: "Довольных клиентов"
+    },
+    {
+      number: 9,
+      icon: FaTrophy,
+      text: "Наград"
+    },
+  ];
+
+  const ref = useRef(null);
+  const isInView = useInView(ref);
 
   return (
     <section className="pt-8 pb-16 lg:pt-16 lg:pb-28 bg-white" id='about'>
@@ -34,7 +62,7 @@ const About = () => {
           <h2 className="h2 text-center">О WellFit</h2>
           <p className="max-w-[600px] mx-auto text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta veritatis unde voluptas non, cum quis autem! Quam nam inventore optio fugit fugiat laudantium nulla quaerat.</p>
         </div>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-12">
           {featureds.map((featured, index) => {
             return (
               <div key={index} className='flex flex-col justify-center items-center gap-4 border p-10 rounded-lg'>
@@ -46,6 +74,27 @@ const About = () => {
               </div>
             )
           })}
+        </div>
+        <div className="w-full">
+          <div className='w-full grid grid-cols-2 md:grid-cols-4'>
+            {stats.map((stat, index) => {
+              return (
+                <div key={index} className="flex flex-col justify-center items-center gap-5">
+                  <div className='border border-accent/90 w-[140px] h-[140px] rounded-full p-1'>
+                    <div ref={ref} className="border border-accent/30 w-full h-full flex items-center justify-center text-5xl rounded-full">
+                      {isInView && (
+                        <CountUp start={0} end={stat.number} duration={6} />
+                      )}
+                    </div>
+                  </div>
+                  <div className='flex flex-col justify-center items-center text-center'>
+                    <stat.icon className="text-3xl mb-2" />
+                    <h4 className='h4'>{stat.text}</h4>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </section>
