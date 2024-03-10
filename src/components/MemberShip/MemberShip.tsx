0a40932e8a2d8b816ce9db3580fe499e1db1d5cf
webNekit <mochalov-hunter@mail.ru>
2024-03-10 11:05:55 +0300

    Доделал слайдер и закончил секцию "программа тренировок"

--- src/components/MemberShip/MemberShip.tsx
+++ src/components/MemberShip/MemberShip.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from 'react';
+import MemberShipSlider from '../MemberShipSlider/MemberShipSlider';
 
 const MemberShip = () => {
   return (
-    <section className='w-full py-16 bg-membership bg-center bg-cover bg-no-repeat' id="memberShip">
+    <section className='w-full py-24 bg-membership bg-center bg-cover bg-no-repeat' id="memberShip">
         <div className="container mx-auto">
             <h2 className="h2 text-white text-center">Программа тренировок</h2>
-            
+            <div className="w-full pt-12">
+              <MemberShipSlider />
+            </div>
         </div>
     </section>
   )
--- src/components/MemberShipSlider/MemberShipSlider.tsx
+++ src/components/MemberShipSlider/MemberShipSlider.tsx
@@ -71,10 +71,37 @@ const MemberShipSlider = () => {
   ];
 
   return (
-    <Swiper>
-      <SwiperSlide>1</SwiperSlide>
-      <SwiperSlide>2</SwiperSlide>
-      <SwiperSlide>3</SwiperSlide>
+    <Swiper className="w-full h-[calc(100% + 120px)]" slidesPerView={1} modules={[ Pagination ]} pagination={{ clickable: true }} breakpoints={{ 768: { slidesPerView: 2, spaceBetween: 30 }, 1024: { slidesPerView: 3, spaceBetween: 30 }  }}>
+      {memberShipData.map((item, index) => {
+        return (
+          <>
+            <SwiperSlide key={index}>
+              <div className="border border-accent hover:bg-primary-300/80 transition-all duration-300 w-full max-w-full lg:max-w-none">
+                <div className="py-5 px-5 border-b border-accent">
+                  <h4 className="h4 text-center text-white">{item.title}</h4>
+                </div>
+                <div className='w-full px-5 py-5'>
+                  <ul className="w-full flex flex-col gap-4">
+                    {item.benefits.map((item, index) => {
+                      return (
+                        <>
+                          <li key={index} className="flex items-center gap-2 text-white">
+                            <item.icon className='text-white/70' />
+                            <span className="text-xl">{item.text}</span>
+                          </li>
+                        </>
+                      );
+                    })}
+                  </ul>
+                </div>
+                <div className="w-full px-5 py-5 border-t border-accent">
+                    <p className="text-center text-2xl text-white uppercase font-bold">{item.price} руб.</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          </>
+        );
+      })}
     </Swiper>
   )
 }
