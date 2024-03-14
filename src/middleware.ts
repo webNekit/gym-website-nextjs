c66ee10fdd0e8316dede291a038bef71d00a09f3
webNekit <mochalov-hunter@mail.ru>
2024-03-14 14:27:02 +0300

    Доделал middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const { isAuthenticated } = getKindeServerSession();
   if (!(await isAuthenticated())) {
-    return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.redirect(new URL('/', request.url))
   }
 }
  
